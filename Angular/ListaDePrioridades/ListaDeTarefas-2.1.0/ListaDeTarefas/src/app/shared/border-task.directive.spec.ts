af1c5fc00da91a09c4c603e0b0ec9a320e2b8b70
Lucas Bez <61715273+AlexandreBez@users.noreply.github.com>
2022-07-23 19:38:29 -0300

    angular

---
+++ Angular/ListaDePrioridades/ListaDeTarefas-2.1.0/ListaDeTarefas/src/app/shared/border-task.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BorderTaskDirective } from './border-task.directive';
+
+describe('BorderTaskDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BorderTaskDirective();
+    expect(directive).toBeTruthy();
+  });
+});
